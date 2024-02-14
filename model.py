3e2c7341d6f6b8e7679b92a807e06ce6229a7d45
Chloe Kaplan <chloekap06@icloud.com>
2024-02-13 20:57:30 -0500

    test

--- model.py
+++ model.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ["GET", "POST"])
+def load_homepage():
+    weather_list = pd.DataFrame([])
+    for df in pd.read_csv('weather.csv', iterator=True, chunksize=1000 ):
+        weather_list = pd.concat([weather_list, pd.DataFrame(df.groupby(["time"]).size().reset_index(name="Count"))])
+    print(weather_list.sort_values(by='Count', ascending = False).head(10))
+    print("Total rows: " + str(sum(weather_list['Count'])))
+    return render_template("index.html")
+
+
+@app.route("/predictor", methods = ["GET", "POST"])
+def load_predictor():
+    return render_template("predictor.html")
