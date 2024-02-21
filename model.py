c18f210fbad368c3f066c445c0b8826a68ef903c
Chloe Kaplan <chloekap06@icloud.com>
2024-02-20 21:02:02 -0500

    get date data

--- model.py
+++ model.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template
 import pandas as pd
 app = Flask(__name__)
-@app.route("/", methods = ["GET", "POST"])
+
+
+@app.route("/", methods=["GET", "POST"])
 def load_homepage():
-    weather_list = pd.DataFrame([])
-    for df in pd.read_csv('weather.csv', iterator=True, chunksize=1000 ):
-        weather_list = pd.concat([weather_list, pd.DataFrame(df.groupby(["time"]).size().reset_index(name="Count"))])
-    print(weather_list.sort_values(by='Count', ascending = False).head(10))
-    print("Total rows: " + str(sum(weather_list['Count'])))
+
+    data = ingest_data('traffic.csv')
+    get_dates(data)
     return render_template("index.html")
 
 
-@app.route("/predictor", methods = ["GET", "POST"])
+@app.route("/predictor", methods=["GET", "POST"])
 def load_predictor():
     return render_template("predictor.html")
+
+
+def ingest_data(csv):
+    data_list = pd.DataFrame([])
+    for chunk in pd.read_csv(csv, iterator=True, chunksize=1000):
+        data_list = pd.concat([data_list, chunk])
+    return data_list.tail(3)
+
+def get_dates(data):
+    date_keywords = ['Date', 'Created Date', 'CRASH DATE', 'time', 'Occured_On' ]
+    for index, row in data.iterrows():
+        for keyword in date_keywords:
+            if keyword in row:
+                print(row[keyword])
